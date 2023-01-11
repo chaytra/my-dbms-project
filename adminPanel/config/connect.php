91400c460d56818e655ea57516ae89888545b6ff
chaytra <84039582+chaytra@users.noreply.github.com>
2023-01-11 23:27:24 +0530

    Update connect.php

--- adminPanel/config/connect.php
+++ adminPanel/config/connect.php
@@ -10,7 +10,7 @@ define('LOCALHOST','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_NAME','food_order');
-define("INDEXPAGE",'http://localhost/rest/');
+define("INDEXPAGE",'http://localhost/dbms/');
 
 // Making connection to database
 $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die("Connection Failed");
